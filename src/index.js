203a64dd90b0f3523b72a7ed055231b502c19acf
Levent Gurses <levent.gurses@64clicks.com>
2017-09-29 20:16:50 -0400

    Redux tools for Chrome

--- src/index.js
+++ src/index.js
@@ -1,32 +1,24 @@
+/* eslint-disable no-unused-vars */
 import './index.css';
 import 'semantic-ui-css/semantic.min.css';
 import { getUsers, deleteUser } from './api/userApi';
-
-/* eslint-disable no-unused-vars */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-
-
+import { createStore, applyMiddleware, compose } from 'redux';
 import App from './components/App';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, /* preloadedState, */ composeEnhancers(
+  applyMiddleware()
+));
 
 ReactDOM.render(
-  <Provider store={ createStoreWithMiddleware(reducers) }>
+  <Provider store={ store }>
     <App />
   </Provider>
   , document.getElementById('root'));
-
-
-
-
-
-
-
-
 
 
 // Populate table of users via API call
