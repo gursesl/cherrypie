cbcdfb56321f016493cb8979823aec225920799f
Levent Gurses <levent.gurses@virtustream.com>
2017-10-01 20:55:23 -0400

    Fix test

---
+++ src/components/UserList/tests/userListComponent.test.js
@@ -0,0 +1,10 @@
+// TODO: Implement this test
+describe('UserList component', () => {
+  it('should render', () => {
+    expect(true).toBeTruthy()
+  })
+
+  it('should do something useful', () => {
+    expect(true).toBeTruthy()
+  })
+})
