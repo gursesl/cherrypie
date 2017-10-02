48e0ed477724e5a26e19b7a001edf3384f7d21f3
Levent Gurses <levent.gurses@virtustream.com>
2017-10-01 21:30:01 -0400

    Container test refactoring

--- src/components/Counter/tests/counterComponent.test.js
+++ src/components/Counter/tests/counterComponent.test.js
@@ -2,25 +2,11 @@
 // REFERENCE: COMPONENT TEST
 import React from 'react'
 import { Provider } from 'react-redux';
-import { mount, unmount, shallow } from 'enzyme'
+import { shallow } from 'enzyme'
 import { expect } from 'chai'
 import sinon from 'sinon'
-import configureStore from 'redux-mock-store'
 import '../../../setupTests'
 import CounterComponent from '../'
-import {
-  INCREMENT_ACTION,
-  DECREMENT_ACTION,
-  INCREMENT_ACTION_ASYNC
-} from '../../../containers/CounterContainer/constants'
-import { incrementAction, decrementAction, incrementActionAsync } from '../../../containers/CounterContainer/actions'
-
-const middlewares = []
-const mockStore = configureStore(middlewares)
-
-// Initialize mock store with empty state
-let initialState = {}
-let store = mockStore(initialState)
 
 const onIncrementSpy = sinon.spy();
 const onDecrementSpy = sinon.spy();
@@ -33,18 +19,18 @@ const props = {
   onIncrementAsync: onIncrementAsyncSpy
 }
 
-let component = mount(<Provider store={store}><CounterComponent {...props} /></Provider>)
+let component = shallow(<CounterComponent {...props} />)
 
 
 describe('<CounterComponent>', () => {
 
-
   beforeEach(() => {
-    store = mockStore(initialState)
-  });
+
+  })
 
   afterEach(() => {
-  });
+
+  })
 
   it('should be available', () => {
     expect(component.find('button').at(1).text()).to.equal('Increment')
@@ -67,31 +53,6 @@ describe('<CounterComponent>', () => {
   it('should find Decrement button and simulate a click', () => {
     component.find('button').at(2).simulate('click')
     expect(onDecrementSpy.calledOnce).to.equal(true);
-
-  })
-
-  it('should dispatch incrementAction', () => {
-    // Dispatch incrementAction
-    store.dispatch(incrementAction())
-
-    // Test if store dispatched the incrementAction action
-    const actions = store.getActions()
-    const expectedPayload = { type: INCREMENT_ACTION }
-    expect(actions).to.deep.equal([expectedPayload])
-  })
-
-  it('should dispatch decrementAction', () => {
-    store.dispatch(decrementAction())
-    const actions = store.getActions()
-    const expectedPayload = { type: DECREMENT_ACTION }
-    expect(actions).to.deep.equal([expectedPayload])
-  })
-
-  it('should dispatch incrementAsyncAction', () => {
-    store.dispatch(incrementActionAsync())
-    const actions = store.getActions()
-    const expectedPayload = { type: INCREMENT_ACTION_ASYNC }
-    expect(actions).to.deep.equal([expectedPayload])
   })
 
 })
