33d923eee07b9cee3844f04a4432f748e041306b
Levent Gurses <levent.gurses@virtustream.com>
2017-10-18 15:17:09 -0400

    https for weather api

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 export const API_URL = 'http://localhost:5000'
 export const MOCK_API_URL = 'http://localhost:3001'
-export const WEATHER_API_URL = 'http://api.openweathermap.org/data/2.5/forecast'
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5/forecast'
 export const MOCK_WEATHER_API_URL = 'http://localhost:3001/searchByCityName'
