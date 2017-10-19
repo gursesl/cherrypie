2a503fbb8c8fda6ef1fff73707b363a2c608d5c3
Levent Gurses <levent.gurses@virtustream.com>
2017-10-19 00:51:49 -0400

    cleanup

--- buildScripts/testSetup.js
+++ buildScripts/testSetup.js
@@ -2,10 +2,10 @@
 global.fetch = require('jest-fetch-mock')
 
 // Register babel to transpile before tests run
-require('babel-register')();
+// require('babel-register')();
 
-// Disable webpack features taht Mocha doesn't understand
-require.extensions.css = () => {};
+// Disable webpack features that Jest doesn't understand
+// require.extensions.css = () => {};
 
 global.requestAnimationFrame = (callback) => {
   setTimeout(callback, 0);
