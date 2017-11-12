c3db2794aa53301e9603d7d91de5b72de9cec54c
Levent Gurses <levent.gurses@virtustream.com>
2017-11-12 14:56:10 -0500

    Prettier

--- src/components/RegisterModal/index.js
+++ src/components/RegisterModal/index.js
@@ -15,23 +15,26 @@ class RegisterModal extends Component {
     failureModalOpen: false,
     errors: [],
   }
-  handleOpen = () => this.setState({
-    modalOpen: true,
-    successModalOpen: false,
-    failureModalOpen: false,
-  })
-  handleClose = () => this.setState({
-    modalOpen: false,
-    successModalOpen: false,
-    failureModalOpen: false,
-  })
+  handleOpen = () =>
+    this.setState({
+      modalOpen: true,
+      successModalOpen: false,
+      failureModalOpen: false,
+    })
+  handleClose = () =>
+    this.setState({
+      modalOpen: false,
+      successModalOpen: false,
+      failureModalOpen: false,
+    })
   handleSuccess = () => this.setState({ modalOpen: false, successModalOpen: true })
   handleFailure = errors => this.setState({ modalOpen: false, failureModalOpen: true, errors })
   handleRegister = (values) => {
     const variables = values.toJS()
-    this.props.mutate({
-      variables,
-    })
+    this.props
+      .mutate({
+        variables,
+      })
       .then((data, errors) => {
         if (data.data.registerUser.id) {
           this.handleSuccess()
@@ -53,35 +56,25 @@ class RegisterModal extends Component {
       <div>
         <Popup trigger={<Button onClick={this.handleOpen}>Sign up</Button>}>
           <Popup.Header>Registration details</Popup.Header>
-          <Popup.Content>
-            Click here to sign up
-          </Popup.Content>
+          <Popup.Content>Click here to sign up</Popup.Content>
         </Popup>
 
-        <Modal
-          open={this.state.modalOpen}
-          onClose={this.handleClose}
-          basic
-          size="small"
-        >
+        <Modal open={this.state.modalOpen} onClose={this.handleClose} basic size="small">
           <Modal.Content>
-            <Grid
-              textAlign="center"
-              style={{ height: '100%' }}
-              verticalAlign="middle"
-            >
+            <Grid textAlign="center" style={{ height: '100%' }} verticalAlign="middle">
               <Grid.Column style={{ maxWidth: 450 }}>
                 <Header as="h2" color="teal" textAlign="center">
-                  <Image src="/img/logo.png" />
-                  {' '}Sign up for a new account
+                  <Image src="/img/logo.png" /> Sign up for a new account
                 </Header>
                 <RegisterForm {...this.props} onSubmit={this.handleRegister} />
                 <Message>
-                  Already have an account? <Link to="/" onClick={this.handleClose}>Sign in</Link>
+                  Already have an account?{' '}
+                  <Link to="/" onClick={this.handleClose}>
+                    Sign in
+                  </Link>
                 </Message>
               </Grid.Column>
             </Grid>
-
           </Modal.Content>
         </Modal>
         <RegistrationSuccessModal
@@ -100,27 +93,27 @@ class RegisterModal extends Component {
 
 const registerMutation = gql`
   mutation registerUser(
-    $password: String!,
-    $fullName: String!,
-    $email: String!,
-    $address:String,
-    $address2: String,
-    $city: String,
-    $state: String,
-    $zip: String,
-    $userType: String!) {
+    $password: String!
+    $fullName: String!
+    $email: String!
+    $address: String
+    $address2: String
+    $city: String
+    $state: String
+    $zip: String
+    $userType: String!
+  ) {
     registerUser(
-      password: $password,
-      fullName: $fullName,
-      email: $email,
-      address: $address,
-      address2: $address2,
-      city: $city,
-      state: $state,
-      zip: $zip,
+      password: $password
+      fullName: $fullName
+      email: $email
+      address: $address
+      address2: $address2
+      city: $city
+      state: $state
+      zip: $zip
       userType: $userType
-    )
-    {
+    ) {
       id
     }
   }
