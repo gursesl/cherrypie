045efa4d3956065ffe2e53dd8c8ce9137f66a212
Levent Gurses <levent.gurses@virtustream.com>
2017-11-15 13:54:52 -0500

    Apollo link upgrade

--- src/apolloClient.js
+++ src/apolloClient.js
@@ -12,12 +12,15 @@ const httpLink = createHttpLink({
 const authLink = setContext(async (_, { headers }) => {
   // get the authentication token from local storage if it exists
   const token = await localforage.getItem('token')
+  const refreshToken = await localforage.getItem('refreshToken')
 
   // return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
       authorization: token ? `Bearer ${token}` : null,
+      'x-token': token,
+      'x-refresh-token': refreshToken,
     },
   }
 })
